51cc477e65b0c60580ef89084f65710000df33ca
Hanna Ferris <hferris@dbq.edu>
2021-03-27 00:56:37 -0700

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Sets up the Express App
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -18,6 +18,9 @@ app.get("/", function (req, res) {
   });
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
   });
   
   // API Routes
