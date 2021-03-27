b54739e4ec21ad9a6ae37d17cff5276fe9bf5e33
Hanna Ferris <hferris@dbq.edu>
2021-03-27 00:12:40 -0700

    added post code, updated ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # This project is my Note-Taker.
-# Check it out here: https://hferris.github.io/Note-Taker/
+# Check it out here: https://github.com/hferris/Note-Taker
 # Check it out here: https://note-takerjkk.herokuapp.com/
 # Here is the screenshot below :star_struck:
 <!-- ![Alt text](/./dist/imgs/snap.shot.png?raw=true "Screenshot") -->.
--- server.js
+++ server.js
@@ -1,16 +1,72 @@
-const express = require("express");
-const app = express();
+// Dependencies
+const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
-const PORT = process.env.PORT || 8080;
+// Sets up the Express App
+const app = express();
+const PORT = 8080;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname));
-
-// use routes
-require('./routes/html.js')(app);
-require('./routes/api.js')(app);
+app.use(express.static("public"));
 
 
-app.listen(PORT, () => console.log("App started on http://localhost:" + PORT));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+  
+  // API Routes
+  app.get('/api/notes', function(req,res) {
+    fs.readFile(path.join(__dirname, './db/db.json'), function(err, data) {
+        console.log(data);
+        res.json(JSON.parse(data));
+    });
+});
+app.post('/api/notes', function(req, res) {
+    const note = {
+        title: req.body.title,
+        text: req.body.text
+    };
+    fs.readFile(path.join(__dirname, './db/db.json'), function(err, data) {
+        console.log("here4")
+        console.log(data);
+        console.log("here after")
+        var parsedNote = JSON.parse(data);
+        parsedNote.push(note);
+        parsedNote = JSON.stringify(parsedNote);
+
+        fs.writeFile('./db/db.json', parsedNote, function(err) {
+            if(err) throw err;
+        });
+        res.redirect('back');
+    });
+});
+app.delete("/api/notes/:id", function (req, res) {
+      const noteId = JSON.parse(req.params.id)
+      console.log(noteId)
+      fs.readFile(path.join(__dirname, './db/db.json'), function (error, notes) {
+        if (error) {
+          return console.log(error)
+        }
+        notes = JSON.parse(notes)
+    
+        notes = notes.filter(val => val.id !== noteId)
+    
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), function (error, data) {
+          if (error) {
+            return error
+          }
+          res.json(notes)
+        })
+      })
+    })
+
+// start server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT: ${PORT}`);
+});
